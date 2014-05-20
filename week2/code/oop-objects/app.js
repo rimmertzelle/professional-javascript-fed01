30604a2950dfa02e7462164b74d14d1e834d4418
Rimmert Zelle <ra_zelle@hotmail.com>
2014-05-20 08:05:48 +0200

    Divided factory function
    Divided the createAthletes factory into createAthlete and
    createAthletes factory function

--- week2/code/oop-objects/app.js
+++ week2/code/oop-objects/app.js
@@ -1,45 +1,52 @@
-/** 
-*	function (simple factory) for creating athletes 
-*	params {json} data example [{name:'Rimmert', surname:'Zelle'},{name:'Gerard'}]
-*/
-var createAthletes = function(data){
-		var athletes = [];
+/**
+ * Function (simple factory) for creating Athletes
+ * Params {json} data example [{name:'Rimmert'},{name:'Gerard'}]
+ */
+var createAthletes = function (data) {
+    var athletes = [];
 
-		[].forEach.call(data, function(val, index, array){
-			
-			/**
-			*	Represents an Athlete
-			*	@param {string} name
-			*	@param {string} surname
-			*/
+    [].forEach.call(data, function (val, index, list) {
+        var athlete = createAthlete(val);
+        athletes.push(athlete);
+    });
 
-			var athlete = {
+    return athletes;
+};
 
-			};
+/**
+ * Factory that creates an Athlete
+ * @param val
+ */
+var createAthlete = function (val) {
+    /**
+     *    Represents an Athlete
+     *    @param {string} name
+     *    @param {string} surname
+     */
+    var athlete = {};
+    Object.defineProperties(athlete, {
+        _name: { //data property
+            value: val.name, //data property attribute; value of the attribute
+            enumerable: false, //data property attribute; you can iterate over this property
+            configurable: true, //data property attribute; is the property changeble
+            writable: true //data property attribute; can the property be written to
+        },
+        name: { //accessor property
+            get: function () { //execute function when property is read
+                console.log('Reading name');
+                return this._name;
+            },
+            set: function (value) { //execute function when property is written
+                console.log('Setting name to %s', value);
+                if (typeof value != "string") {
+                    value = "";
+                }
+                this._name = value;
+            },
+            enumerable: true,
+            configurable: true
+        }
+    });
 
-			Object.defineProperties(athlete, {
-
-				_name:{ //data property
-					value: val.name, //data property attribute; value of the attribute
-					enumerable: true, //data property attribute; you can iterate over this property
-					configurable: true, //data property attribute; is the property changeble
-					writable: true //data property attribute; can the property be written to
-				},
-				name:{ //accessor property
-					get:function(){ //execute function when property is read
-						console.log('Reading name');
-						return this._name;
-					},
-					set:function(value){ //execute function when property is written
-						console.log('Setting name to %s', value);
-						this._name = value;
-					},
-					enumerable:true,
-					configurable:true
-				},
-			});
-
-			athletes.push(athlete);
-		});
-	return athletes;
+    return athlete;
 };
--- week2/code/oop-objects/athlete.js
+++ week2/code/oop-objects/athlete.js
@@ -1,48 +1,51 @@
 /**
-*	Represents an Athlete
-*	@param {string} name
-*	@param {string} surname
-*	Commenting style: JSDoc
-*/
+ *    Represents an Athlete
+ *    @param {string} name
+ *    @param {string} surname
+ *    Commenting style: JSDoc
+ */
 
 var athlete = {};
 
 Object.defineProperties(athlete, {
+    _name: {
+        value: "Rimmert",
+        enumerable: false,
+        configurable: true,
+        writable: true
+    },
+    _surname: {
+        value: "Zelle",
+        enumerable: false,
+        configurable: true,
+        writable: true
+    },
+    name: {
+        get: function () {
+            console.log('Reading name');
+            return this._name;
+        },
+        set: function (value) {
+            if (typeof value != "string") {
+                value = "";
+            }
 
-	_name:{
-		value: "Rimmert", 
-		enumerable: true,
-		configurable: true,
-		writable: true
-	},
-	_surname:{
-		value: "Zelle",
-		enumerable: true,
-		configurable: true,
-		writable: true
-	},
-	name:{
-		get:function(){
-			console.log('Reading name');
-			return this._name;
-		},
-		set:function(){
-			console.log('Setting name to %s', value);
-			this._name = value;
-		},
-		enumerable:true,
-		configurable:true
-	},
-	surname:{
-		get:function(){
-			console.log('Reading name');
-			return this._surname;
-		},
-		set:function(){
-			console.log('Setting name to %s', value);
-			this._surname = value;
-		},
-		enumerable:true,
-		configurable:true
-	}
+            console.log('Setting name to %s', value);
+            this._name = value;
+        },
+        enumerable: true,
+        configurable: true
+    },
+    surname: {
+        get: function () {
+            console.log('Reading name');
+            return this._surname;
+        },
+        set: function (value) {
+            console.log('Setting name to %s', value);
+            this._surname = value;
+        },
+        enumerable: true,
+        configurable: true
+    }
 });
